28af9d9441604a2bb53fe1a4dbfe4cb7cf00aecc
danivsainz <danivsainz@gmail.com>
2018-02-05 16:17:38 -0500

    Create test for case where one player should be able to enter

--- lottery/test/lottery.test.js
+++ lottery/test/lottery.test.js
@@ -16,5 +16,25 @@ beforeEach(async () => {
     lottery = await new web3.eth.Contract(JSON.parse(interface))
                         .deploy({data: bytecode})
                         .send({from: accounts[0], gas: '1000000'});
+    lottery.setProvider(provider);
 });
 
+describe('Lottery Contract', () => {
+    it('deploys a contract', () => {
+        assert.ok(lottery.options.address);
+    });
+
+    it('Allows one account to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length)
+    });
+});
