6757a4b19b724845a8d7f57a157946dd18d64bab
danivsainz <danivsainz@gmail.com>
2018-02-05 14:53:58 -0500

    add modifier function and getplayers

--- inbox/contracts/lottery_test.sol
+++ inbox/contracts/lottery_test.sol
@@ -17,8 +17,19 @@ contract Lottery{
         return uint(keccak256(block.difficulty, now, players));
     }
     
-    function pickWinner() public {
+    function pickWinner() public restricted {
+
         uint index = random() % players.length;
         players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+    
+    function getPlayers() public view {
+        
+    }
+    
+    modifier restricted(){
+        require(msg.sender == manager );
+        _;
     }
 }
