43feb196366bc78dda72a1a21c3b44748f998c46
danivsainz <danivsainz@gmail.com>
2018-02-04 23:10:45 -0500

    Create small test sample in mocha

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -3,3 +3,21 @@ const ganache = require('ganache-cli');
 // Web3 is in caps is because it's a constructor function
 const Web3 = require('web3');
 
+const web3 = new Web3(ganache.provider());
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    vroom(){
+        return 'vroom';
+    }
+}
+
+describe('Car', () => {
+    it('can park', ()=> {
+        const car = new Car();
+        assert.equal(car.park(), 'stopped');
+    })
+});
