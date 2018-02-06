8db6f317f854abd35a83ab8f39df0f168d64b46c
danivsainz <danivsainz@gmail.com>
2018-02-06 00:30:14 -0500

    begin filling searchbar

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search-bar'
+
 const API_KEY = "AIzaSyCjOr5G7ASBX7kMi5jFlEYaGBow3KoGklo";
 
 //Create a now component that generates html
 const App =  () => {
-    return <div>Hi</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 //Take this components generated html and put it in the dom
