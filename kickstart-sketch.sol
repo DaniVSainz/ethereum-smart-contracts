487898cecc6b20df3ffd4f55e4104c7610594195
danivsainz <danivsainz@gmail.com>
2018-02-08 01:10:43 -0500

    Kickstart

--- kickstart-sketch.sol
+++ kickstart-sketch.sol
@@ -42,7 +42,18 @@ contract Campaign {
            complete: false,
            approvalCount: 0
         });
-        
         requests.push(newRequest);
     }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+        
+    }
+    
 }
