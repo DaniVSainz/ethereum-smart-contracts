1e3418dd9e607445273c5493f7ae57a3af86586c
danivsainz <danivsainz@gmail.com>
2018-02-07 22:23:19 -0500

    More code

--- kickstart-sketch.sol
+++ kickstart-sketch.sol
@@ -1,10 +1,25 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    //Struct is like creating a new type with its descriptions, essentially like a model
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    Request [] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
     
+    //Modifies functions
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    // Constructor function
     function Campaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
