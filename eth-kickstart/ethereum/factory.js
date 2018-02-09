5ebfb9a857103ee6f0a8ef20314674cf75bdd1ed
danivsainz <danivsainz@gmail.com>
2018-02-09 05:16:18 -0500

    Work across react impl in web3 and factory

--- eth-kickstart/ethereum/factory.js
+++ eth-kickstart/ethereum/factory.js
@@ -1,8 +1,8 @@
 import web3 from './web3';
 import compiledContract from './build/CampaignFactory.json';
 
-const instace = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
+const instance = new web3.eth.Contract(
+    JSON.parse(compiledContract.interface),
     '0xbcc3c2AB53AE3cDAB814C9C4A3c00c51Ff8e7761'
 );
 
--- eth-kickstart/ethereum/web3.js
+++ eth-kickstart/ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(windwo.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // in the browser and metamask is running
+     web3 = new Web3(window.web3.currentProvider);
+}else {
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/Xl5rmlE4KRpT4dcuJpBl'
+    );
+     web3 = new Web3(provider);  
+}
+
 
 export default web3;
