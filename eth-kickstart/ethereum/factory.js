882f84d41b28b4df10271eb396e3aa14984d6cf1
danivsainz <danivsainz@gmail.com>
2018-02-10 06:46:06 -0500

    Modify Contract, re compile,re deploy , & set new adress in factory.js

--- eth-kickstart/ethereum/factory.js
+++ eth-kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import compiledContract from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(compiledContract.interface),
-    '0xbcc3c2AB53AE3cDAB814C9C4A3c00c51Ff8e7761'
+    '0x972aDF6B99715FE020c35a6d907324309dAb0e83'
 );
 
 export default instance;
